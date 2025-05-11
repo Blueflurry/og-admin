c02d987562c66650cbca901e3ca8883ed6d4d72a
siddhantj929 <siddhantj929@gmail.com>
2025-05-12 05:01:28 +0530

    added new domain url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const baseURL = "https://sharing-sponge-forcibly.ngrok-free.app/api";
 // const baseURL = "http://localhost:4000/api";
-const baseURL = "https://jaro-connect-backend.onrender.com/api";
+const baseURL = "https://oblik.ddns.net/api";
 
 const instance = axios.create({ baseURL });
 // const instance = axios.create({ baseURL, withCredentials: true });
